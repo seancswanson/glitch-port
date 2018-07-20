ac46d4f60e0f749ec6eb1a20039e6549d0710490
Sean Swanson <sean-swan@hotmail.com>
2018-07-20 12:19:22 -0700

    Cascading hierarchy

--- js/script.js
+++ js/script.js
@@ -1,36 +1,31 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
-
-// prints "hi" in the browser's dev tools console
-
 (function() {
-  
+
   window.swanson = {
-   
+
     // ----------
     init: function() {
       this.$textBox = $(document).find('.hello-hi');
       this.$textBoxAbout = $(document).find('.main-about-me');
       console.log(this.$textBox);
-      
+
       this.textBoxController();
-      
+
     },
-    
+
     textBoxController: function() {
         $(document).find('.main--paragraph__first').toggleClass('hidden');
-      
+
       this.$textBox.on('click', function() {
         $(document).find('.main--paragraph__first').toggleClass('hidden');
         $(document).find('.main--paragraph__second').toggleClass('hidden');
-      });   
-      
+      });
+
       this.$textBoxAbout.on('click', function() {
         $(document).find('.main--paragraph__first').toggleClass('hidden');
         $(document).find('.main--paragraph__second').toggleClass('hidden');
       })
-    } 
+    }
   }
-  
+
   swanson.init();
 })()
